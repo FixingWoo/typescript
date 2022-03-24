5ddbbdf6608a698c7d082e96b2e3b3703cefd6d7
fixingwoo <fixingwoo@gmail.com>
2022-03-25 00:34:47 +0900

    feat: Forms bind events

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { 
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="yellow" />
-      <Circle bgColor="gray" borderColor="purple" text="hello"/>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          placeholder="username" 
+          value={value}
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
